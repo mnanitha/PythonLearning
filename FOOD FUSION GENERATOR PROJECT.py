32864a4da5e3d07c6fa92ffbb669da3fe829276d
mnanitha <mn.anitha@gmail.com>
2024-10-28 20:46:22 -0700

    first file

---
+++ FOOD FUSION GENERATOR PROJECT.py
@@ -0,0 +1,7 @@
+print("Welcome to Food Fusion")
+Cuisine=input("Enter your favorite cuisine: ")
+print(Cuisine)
+fruit=input("Enter your favorite fruit: ")
+print(fruit)
+Fav=f"Here comes your favorites: {Cuisine}{fruit}!"
+print(Fav)
